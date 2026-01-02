130a4ddf17c9e56731b9ceb5a5b04bf8ef1218d8
PSBVISION <punitsinghbisht1529@gmail.com>
2026-01-02 20:10:04 +0530

    feat(auth): implement authentication middleware and integrate with messages endpoint

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,6 +1,8 @@
 import { redis } from "@/lib/redis";
 import { Elysia, t } from "elysia";
 import { nanoid } from "nanoid";
+import { authMiddleware } from "./auth";
+import z from "zod";
 
 const ROOM_TTL_SECONDS = 60 * 10; // 10 minutes
 
@@ -13,6 +15,27 @@ const rooms = new Elysia({ prefix: "/room" }).post("/create", async () => {
   await redis.expire(`meta:${roomId}`, ROOM_TTL_SECONDS);
   return { roomId };
 });
+
+const messages = new Elysia({ prefix: "/messages" }).use(authMiddleware).post(
+  "/",
+  ({ body, auth }) => {
+    const {roomId} = auth;
+    const { sender, text } = body;
+    const roomExists = await redis.exists(`meta:${roomId}`)
+    if(!roomExists){
+      throw new Error("Room does not exist");
+    }
+    
+  },
+  {
+    query: z.object({roomId: z.string()}),
+    body: z.object({
+      sender: z.string().max(100),
+      text: z.string().max(1000),
+    }),
+  }
+);
+
 const app = new Elysia({ prefix: "/api" }).use(rooms);
 
 export const GET = app.fetch;
